56f3d5942fdb1e0f38067f3e7681cfb83eff19df
zli786 <Teng@2014010497>
2021-04-22 00:08:59 +1200

    Add icon button on the map

--- frontend/src/components/PostPlaza/PostsOnMap/PostsOnMap.js
+++ frontend/src/components/PostPlaza/PostsOnMap/PostsOnMap.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from "react";
-import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import React, {useState, useEffect} from "react";
+import ReactMapGL, {Marker, Popup} from "react-map-gl";
 import IconButton from '@material-ui/core/IconButton';
-import PetsIcon from "@material-ui/icons/Pets";
+import PetsIcon from '@material-ui/icons/Pets'
 
 
-export default function PostsOnMap({post}){
+export default function PostsOnMap({posts}) {
     const [viewport, setViewport] = useState({
         latitude: -36.848461,
         longitude: 174.763336,
@@ -12,49 +12,50 @@ export default function PostsOnMap({post}){
         height: "500px",
         zoom: 10
     });
-    const [selectedPetPoint, setSelectedPetPoint] = useState(null);
-    // const lastSpot = post.trace[post.trace.length - 1]
-
-    useEffect(() => {
-        const listener = e => {
-            if (e.key === "Escape") {
-                setSelectedPetPoint(null);
-            }
-        };
-        window.addEventListener("keydown", listener);
-
-        return () => {
-            window.removeEventListener("keydown", listener);
-        };
-    }, []);
-
+    // const [selectedPetPoint, setSelectedPetPoint] = useState(null);
+    //
+    // useEffect(() => {
+    //     const listener = e => {
+    //         if (e.key === "Escape") {
+    //             setSelectedPetPoint(null);
+    //         }
+    //     };
+    //     window.addEventListener("keydown", listener);
+    //
+    //     return () => {
+    //         window.removeEventListener("keydown", listener);
+    //     };
+    // }, []);
+    //
 
     return (
         <div>
             <ReactMapGL
                 {...viewport}
                 mapboxApiAccessToken='pk.eyJ1IjoiemxpNzg2IiwiYSI6ImNrbnF1NzcyYjBkcnAydm4wenhvN2J0YmEifQ.QU5fBqJ3Gy7vvu9xWEMIKg'
-                    mapStyle="mapbox://styles/zli786/cknqui4de047a18qrip6y1kib"
+                mapStyle="mapbox://styles/zli786/cknqui4de047a18qrip6y1kib"
                 onViewportChange={viewport => {
                     setViewport(viewport);
                 }}
             >
-                {/*{post.trace.map(post => (*/}
-                {/*    <Marker*/}
-                {/*        key={post.properties.PARK_ID}*/}
-                {/*        latitude={post.trace.latitude}*/}
-                {/*        longitude={post.trace.longitude}*/}
-                {/*    >*/}
-                {/*        <IconButton color="inherit"*/}
-                {/*                    className="marker-btn"*/}
-                {/*                    onClick={e => {*/}
-                {/*                        e.preventDefault();*/}
-                {/*                        setSelectedPetPoint(post);}}*/}
-                {/*        >*/}
 
-                {/*            <PetsIcon />*/}
-                {/*        </IconButton>*/}
-                {/*    </Marker>*/}
+                <Marker
+                    // key={post._id}
+                    latitude={-36.912300}
+                    longitude={174.739000}
+                >
+                    <IconButton edge="start"  color="inherit" aria-label="menu">
+                        <PetsIcon />
+                    </IconButton>
+
+                    {/*onClick={e => {*/}
+                    {/*e.preventDefault();*/}
+                    {/*setSelectedPetPoint(post);}}*/}
+                </Marker>
+
+                {/*{posts.map(post => (*/}
+
+                {/* */}
                 {/*))}*/}
 
                 {/*{selectedPetPoint ? (*/}
