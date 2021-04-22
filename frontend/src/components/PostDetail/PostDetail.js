734efcde2abb03abac80fe766ce60c37bd5f5eeb
zli786 <Teng@2014010497>
2021-04-22 21:41:39 +1200

    Show map on Post Detail

--- frontend/src/components/PostDetail/PostDetail.js
+++ frontend/src/components/PostDetail/PostDetail.js
@@ -1,30 +1,36 @@
 import React from 'react'
 import {useHistory, useParams} from 'react-router-dom'
 import PetImages from "../PostPlaza/Posts/PostCard/PetImages/PetImages";
+import PostDetailOnMap from "./PostDetailOnMap/PostDetailOnMap";
 
-export default function PostDetail(){
-        const location = useHistory().location
-        const post = location.state
-    return(
+export default function PostDetail() {
+    const location = useHistory().location
+    const post = location.state
+    return (
         <div>
-            <li>{post._id}</li>
-            <PetImages urls={post.petImages}/>
-            <li>{post.petName}</li>
-            <li>{post.petType.species}</li>
-            <li>{post.petType.breed}</li>
-            <li>{post.petColor}</li>
-            <li>{post.petSize}</li>
-            <li>{post.petGender}</li>
-            <li>{post.isMicroshipped}</li>
-            <li>{post.microchipNumber}</li>
-            <li>{post.desexed}</li>
-            <li>{post.collarTagDescription}</li>
-            <li>{post.comment}</li>
-            <li>{post.status}</li>
-                <li>{post.trace.map(spot=>{
+            <div>
+                <li>{post._id}</li>
+                <PetImages urls={post.petImages}/>
+                <li>{post.petName}</li>
+                <li>{post.petType.species}</li>
+                <li>{post.petType.breed}</li>
+                <li>{post.petColor}</li>
+                <li>{post.petSize}</li>
+                <li>{post.petGender}</li>
+                <li>{post.isMicroshipped}</li>
+                <li>{post.microchipNumber}</li>
+                <li>{post.desexed}</li>
+                <li>{post.collarTagDescription}</li>
+                <li>{post.comment}</li>
+                <li>{post.status}</li>
+                <li>{post.trace.map(spot => {
                     <span>(${spot.latitude},${spot.longitude})</span>
                 })}</li>
                 <li>{post.createdAt}</li>
+            </div>
+            <div>
+                <PostDetailOnMap post={post}/>
+            </div>
         </div>
-        )
+    )
 }
--- frontend/src/components/PostDetail/PostDetailOnMap/PostDetailOnMap.js
+++ frontend/src/components/PostDetail/PostDetailOnMap/PostDetailOnMap.js
@@ -1,8 +1,29 @@
 import React, {useState, useEffect} from "react";
 import ReactMapGL, {Marker, Popup} from "react-map-gl";
-import {useHistory} from "react-router-dom";
+// import {useHistory} from "react-router-dom";
 
-export default function PostDetailOnMap(){
+export default function PostDetailOnMap({post}) {
+    const [viewport, setViewport] = useState({
+        latitude: -36.848461,
+        longitude: 174.763336,
+        width: "100%",
+        height: "500px",
+        zoom: 10
+    });
 
-    
+    return (
+        <div>
+            <ReactMapGL
+                {...viewport}
+                mapboxApiAccessToken='pk.eyJ1IjoiemxpNzg2IiwiYSI6ImNrbnF1NzcyYjBkcnAydm4wenhvN2J0YmEifQ.QU5fBqJ3Gy7vvu9xWEMIKg'
+                mapStyle="mapbox://styles/zli786/cknqui4de047a18qrip6y1kib"
+                onViewportChange={viewport => {
+                    setViewport(viewport);
+                }}
+            >
+
+            </ReactMapGL>
+
+        </div>
+    )
 }
