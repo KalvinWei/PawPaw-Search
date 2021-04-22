e1e925d1c32d2a220724fdd208a6ca5eabff826f
zli786 <Teng@2014010497>
2021-04-22 21:09:19 +1200

    Create PostDetailOnMap.js module.

---
+++ frontend/src/components/PostDetail/PostDetailOnMap/PostDetailOnMap.js
@@ -0,0 +1,8 @@
+import React, {useState, useEffect} from "react";
+import ReactMapGL, {Marker, Popup} from "react-map-gl";
+import {useHistory} from "react-router-dom";
+
+export default function PostDetailOnMap(){
+
+    
+}
